7819ea808893bd44be551e5748a73c76d67440fe
Adam Halim <adaha@cendio.se>
2024-12-06 08:30:49 +0100

    TEMP: suite: Add FrameInStream class
    This class is used to parse the output generated from a FrameOutStream.

---
+++ tests/suite/io/FrameInStream.cxx
@@ -0,0 +1,68 @@
+#include "FrameInStream.h"
+#include "../codec/decoderFactory.h"
+#include <ios>
+#include <iostream>
+#include <assert.h>
+
+namespace suite {
+
+  FrameInStream::FrameInStream()
+    : decoder_(nullptr), headerParsed(false)
+  {
+  }
+
+  FrameInStream::~FrameInStream()
+  {
+    delete decoder_;
+  }
+
+  Image* FrameInStream::readImage(std::istream& is)
+  {
+    assert(headerParsed);
+
+    int size, width, height, x_offset, y_offset;
+    double frameTime;
+    ImageUpdateStats stats;
+
+    if (is.peek() == EOF)
+      throw std::ios_base::failure("error reading from stream");
+
+    is >> size >> width >> height >> x_offset >> y_offset >> frameTime
+       >> stats.lostDataArea >> stats.overDimensionedArea
+       >> stats.encodingTime >> stats.margin;
+    is.ignore();
+
+    uint8_t* data = new uint8_t[size];
+    is.read((char*)data, size);
+
+    Image* image = decoder_->decodeImageFromMemory(data, width, height,
+                                                  size, x_offset,
+                                                  y_offset);
+    delete [] data;
+    image->frameTime_ = frameTime;
+    image->stats_ = stats;
+    return image;
+  }
+
+  HeaderData FrameInStream::parseHeader(std::istream& is)
+  {
+    assert(!headerParsed);
+
+    std::string decoder;
+    int width;
+    int height;
+    double interval;
+    is >> decoder >> width >> height >> interval;
+
+    HeaderData header {
+      .width = width,
+      .height = height,
+      .interval = interval,
+      .decoder = decoder,
+    };
+
+    decoder_ = constructDecoder(decoder);
+    headerParsed = true;
+    return header;
+  }
+}
---
+++ tests/suite/io/FrameInStream.h
@@ -0,0 +1,40 @@
+#ifndef __SUITE_FRAME_IN_STREAM_H__
+#define __SUITE_FRAME_IN_STREAM_H__
+
+#include "../codec/ImageDecoder.h"
+namespace suite {
+
+  struct HeaderData {
+    int width;
+    int height;
+    double interval;
+    std::string decoder;
+  };
+
+  // This class parses an istream that contains data following the
+  // structure defined in FrameOutStream.
+  //
+  // FIXME: Probably want to wrap this class so that it is more seamless
+  // to use. We shouldn't have to call parseHeader() before readImage().
+  class FrameInStream
+  {
+  public:
+    FrameInStream();
+    ~FrameInStream();
+
+    // Parses one image from the isteam.
+    // Should be called repeatedly in a loop to parse an entire file.
+    Image* readImage(std::istream& is);
+
+    // Parses the header of the file and returns a pair with the
+    // width and size of the framebuffer.
+    // Needs to be run before readImage as it also sets the decoder.
+    HeaderData parseHeader(std::istream& is);
+  protected:
+    ImageDecoder* decoder_;
+  private:
+    bool headerParsed;
+  };
+}
+
+#endif // __SUITE_FRAME_IN_STREAM_H__
