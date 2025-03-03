adamhalim/tigervnc-suite

TEMP: Add X display framebuffer update recorder
The Recorder is used to record an X session to file. The class uses the
XDAMAGE extensions and is meant to mimic how x0vncserver is used to
track changes/framebuffer updates.

tests/suite/codec/ImageDecoder.h
@@ -1,0 +1,61 @@
+#ifndef __SUITE_IMAGE_DECODER_H__
+#define __SUITE_IMAGE_DECODER_H__
+
+#include "Image.h"
+#include <chrono>
+#include <map>
+
+namespace suite {
+
+  enum DecoderEnum {
+    JPEG = 0,
+  };
+
+  static std::map<std::string, DecoderEnum> decodersMap = {
+    {"JPEG", JPEG}
+  };
+
+  static std::string decoderTypeToString(enum DecoderEnum e)
+  {
+    static std::string strings[] = {
+      "JPEG",
+    };
+    return strings[e];
+  }
+
+  class ImageDecoder
+  {
+  public:
+    ImageDecoder(DecoderEnum type)
+      : type(type), name(decoderTypeToString(type))
+    {
+#ifdef _DEBUG
+        start = std::chrono::system_clock::now();
+        pixelCount = 0;
+#endif // _DEBUG
+    };
+    virtual ~ImageDecoder() {};
+
+    virtual Image *decodeImageFromFile(std::string filename) = 0;
+    virtual Image* decodeImageFromMemory(uint8_t* data, int width, int height,
+                                         int size, int x_offest = 0,
+                                         int y_offset = 0) = 0;
+    virtual void encodeImageTofile(const uint8_t* data, int width, int height,
+                                   std::string filename) = 0;
+    virtual Image* encodeImageToMemory(const uint8_t* data, int width,
+                                       int height, int offset_x = 0,
+                                       int offset_y  = 0) = 0;
+    const DecoderEnum type;
+    const std::string name;
+  protected:
+#ifdef _DEBUG
+    // Measures encoding/decoding performance.
+    void measurePixelRate(int width, int height, int channels);
+  private:
+    unsigned long pixelCount;
+    std::chrono::system_clock::time_point start;
+#endif // _DEBUG
+  };
+}
+
+#endif // __SUITE_IMAGE_DECODER_H__

tests/suite/codec/JPEGDecoder.cxx
@@ -1,0 +1,103 @@
+#include "JPEGDecoder.h"
+#include "ImageDecoder.h"
+#include <rfb/JpegCompressor.h>
+#include <rfb/JpegDecompressor.h>
+#include <rfb/ServerParams.h>
+#include <rfb/TightDecoder.h>
+#include <rfb/TightJPEGEncoder.h>
+#include <fstream>
+
+namespace suite {
+  rfb::PixelFormat pf(32, 24, false, true,
+                      255, 255, 255, 0, 8, 16);
+
+  JPEGDecoder::JPEGDecoder(int quality, int subsampling)
+    : ImageDecoder(JPEG), quality(quality), subsampling(subsampling)
+  {
+  }
+
+  JPEGDecoder::~JPEGDecoder()
+  {
+  }
+
+
+  Image* JPEGDecoder::decodeImageFromFile(std::string)
+  {
+    throw std::logic_error("method not implemented");
+  }
+
+  Image* JPEGDecoder::decodeImageFromMemory(uint8_t *data, int width,
+                                            int height, int size,
+                                            int x_offset, int y_offset)
+  {
+    rfb::Rect rect;
+    uint8_t* buffer;
+    Image* image;
+    rfb::JpegDecompressor jd;
+
+    rect.tl.x = x_offset;
+    rect.tl.y = y_offset;
+    rect.br.x = x_offset + width;
+    rect.br.y = y_offset + height;
+
+    buffer = new uint8_t[width * height * 4];
+
+    jd.decompress(data, size, buffer, width, rect, pf);
+
+#ifdef _DEBUG
+    measurePixelRate(width, height, 4);
+#endif // _DEBUG
+
+    image = new Image(width, height, buffer, size, x_offset, y_offset);
+    return image;
+  }
+
+  void JPEGDecoder::encodeImageTofile(const uint8_t *data, int width,
+                                      int height, std::string filename)
+  {
+    rfb::Rect rect;
+    std::ofstream of;
+    rfb::JpegCompressor jc;
+
+    rect.tl.x = 0;
+    rect.tl.y = 0;
+    rect.br.x = width;
+    rect.br.y = height;
+
+    jc.clear();
+    jc.compress(data, width, rect, pf, quality, subsampling);
+
+    of.open(filename);
+    of.write((char*)jc.data(), jc.length());
+    of.close();
+  }
+
+  Image* JPEGDecoder::encodeImageToMemory(const uint8_t *data, int width,
+                                          int height, int x_offset,
+                                          int y_offset)
+  {
+    rfb::Rect rect;
+    uint8_t *bufferCopy;
+    rfb::JpegCompressor jc;
+    Image* image;
+
+    rect.tl.x = x_offset;
+    rect.tl.y = y_offset;
+    rect.br.x = x_offset + width;
+    rect.br.y = y_offset + height;
+
+    jc.clear();
+    jc.compress(data, width, rect, pf, quality, subsampling);
+
+    bufferCopy = new uint8_t[jc.length()];
+    memcpy(bufferCopy, jc.data(), jc.length());
+
+#ifdef _DEBUG
+    measurePixelRate(width, height, 4);
+#endif // _DEBUG
+
+    image = new Image(width, height, bufferCopy, jc.length(),
+                             x_offset, y_offset);
+    return image;
+  }
+}

tests/suite/io/FrameOutStream.h
@@ -1,0 +1,93 @@
+#ifndef __SUITE_FRAME_OUT_STREAM_H__
+#define __SUITE_FRAME_OUT_STREAM_H__
+
+#include "ImageUpdate.h"
+#include "../codec/ImageDecoder.h"
+#include <fstream>
+#include <mutex>
+
+namespace suite {
+
+  /* This class is used to create the output file of a recorded VNC session.
+     The file structure is as follows:
+
+      ______________________________________________________
+      |               encoding width height interval     \n| <-- File header
+      | imageSize width height x_offset y_offset frameTime | <-- Image metadata
+      |       lostArea overArea encodingTime margin      \n|
+      |****************************************************|
+      |****************************************************|
+      |****************** RAW IMAGE DATA ******************| <-- Raw dump of
+      |****************************************************|     image data.
+      |****************************************************|
+      |**************************************************\n|
+      | imageSize width height x_offset y_offset frameTime | <-- Next image
+      |       lostArea overArea encodingTime margin      \n|
+      |****************************************************|
+      |****************************************************|
+      |****************** RAW IMAGE DATA ******************|
+      |**************************************************\n|
+      |____________________________________________________|
+
+      FileStructure: {
+        encoding:     Which encoder was used.
+        width:        Framebuffer width.
+        height:       Framebuffer height.
+        interval:     Specified interval between frame updates (milliseconds).
+        -----------------------------------------------------------------------
+        imageSize:    Image size in bytes.
+        width:        Image width.
+        height:       Image height.
+        x_offset:     Image top left x-coordinate.
+        y_offset:     Image top left y-coordinate.
+        frameTime:    Time until next frame update.
+
+        lostArea:     How large of an "area" that was lost due to multiple
+                      DAMAGE events detected between frame updates.
+
+        overArea:     How much area was overdimensioned by coalescing multiple
+                      DAMAGE events into a single bouding retangle.
+
+        encodingTime: How long it took to encode the update (milliseconds).
+
+        margin:       How many milliseconds we have to spare before the next
+                      framebuffer update is recorded. Negative number indicates
+                      how long we exceeded our interval.
+        -----------------------------------------------------------------------
+        RAW IMAGE DATA: encoded image data.
+      }
+
+      The file metadata tells which encoding was used for the images, as well
+      as the framebuffer width & height, as well as the interval between each
+      frame update. The header must be parsed first before processing the rest
+      of the file. The file header ends with a '\n'. The image header has
+      information about the next image stored in the file and is also marked by
+      ending with a '\n'. On the next line, the image can be read by reading
+      imageSize many bytes from the file, using the corresponding encoder.
+      The raw image is also terminated with a '\n'. There is no way of knowing
+      how many images are stores in the file beforehand, you have to parse the
+      whole file to know that.
+  */
+
+  class FrameOutStream
+  {
+  public:
+    FrameOutStream(std::string filename, ImageDecoder* decoder);
+    ~FrameOutStream();
+
+    // Assumes ownership if ImageUpdate
+    void addUpdate(ImageUpdate* update);
+    void addUpdate(uint8_t* data, int width, int height, int x_offset,
+                   int y_offset, int size);
+
+    void writeHeader(int width, int height, double interval);
+  private:
+    std::ofstream file;
+    std::mutex lock; // In case updates are encoded in parallel.
+    bool headerWritten;
+    const std::string decoder;
+    std::chrono::steady_clock::time_point lastFrameTime;
+  };
+}
+
+#endif // __SUITE_FRAME_OUT_STREAM_H__

tests/suite/io/ImageUpdate.cxx
@@ -1,0 +1,13 @@
+#include "ImageUpdate.h"
+
+namespace suite {
+  ImageUpdate::ImageUpdate(Image* image, ImageUpdateStats stats)
+    : image(image), stats(stats)
+  {
+  }
+
+  ImageUpdate::~ImageUpdate()
+  {
+    delete image;
+  }
+}

tests/suite/io/ImageUpdate.h
@@ -1,0 +1,19 @@
+#ifndef __SUITE_IMAGE_UPDATE_H__
+#define __SUITE_IMAGE_UPDATE_H__
+
+#include "../codec/Image.h"
+
+namespace suite {
+
+  // Represents a framebuffer update. Takes ownership of the image.
+  // FIXME: Add time data so that updates can be played in "realtime".
+  struct ImageUpdate
+  {
+    ImageUpdate(Image* image, ImageUpdateStats stats = ImageUpdateStats{});
+    ~ImageUpdate();
+    Image* image;
+    ImageUpdateStats stats;
+  };
+}
+
+#endif // __SUITE_IMAGE_UPDATE_H__

madnicendio/tigervnc

tests/suite/codec/ImageDecoder.h
@@ -1,0 +1,61 @@
+#ifndef __SUITE_IMAGE_DECODER_H__
+#define __SUITE_IMAGE_DECODER_H__
+
+#include "Image.h"
+#include <chrono>
+#include <map>
+
+namespace suite {
+
+  enum DecoderEnum {
+    JPEG = 0,
+  };
+
+  static std::map<std::string, DecoderEnum> decodersMap = {
+    {"JPEG", JPEG}
+  };
+
+  static std::string decoderTypeToString(enum DecoderEnum e)
+  {
+    static std::string strings[] = {
+      "JPEG",
+    };
+    return strings[e];
+  }
+
+  class ImageDecoder
+  {
+  public:
+    ImageDecoder(DecoderEnum type_)
+      : type(type_), name(decoderTypeToString(type_))
+    {
+#ifdef _DEBUG
+        start = std::chrono::system_clock::now();
+        pixelCount = 0;
+#endif // _DEBUG
+    };
+    virtual ~ImageDecoder() {};
+
+    virtual Image *decodeImageFromFile(std::string filename) = 0;
+    virtual Image* decodeImageFromMemory(uint8_t* data, int width, int height,
+                                         int size, int x_offest = 0,
+                                         int y_offset = 0) = 0;
+    virtual void encodeImageTofile(const uint8_t* data, int width, int height,
+                                   std::string filename) = 0;
+    virtual Image* encodeImageToMemory(const uint8_t* data, int width,
+                                       int height, int offset_x = 0,
+                                       int offset_y  = 0) = 0;
+    const DecoderEnum type;
+    const std::string name;
+  protected:
+#ifdef _DEBUG
+    // Measures encoding/decoding performance.
+    void measurePixelRate(int width, int height, int channels);
+  private:
+    unsigned long pixelCount;
+    std::chrono::system_clock::time_point start;
+#endif // _DEBUG
+  };
+}
+
+#endif // __SUITE_IMAGE_DECODER_H__

tests/suite/codec/JPEGDecoder.cxx
@@ -1,0 +1,103 @@
+#include "JPEGDecoder.h"
+#include "ImageDecoder.h"
+#include <rfb/JpegCompressor.h>
+#include <rfb/JpegDecompressor.h>
+#include <rfb/ServerParams.h>
+#include <rfb/TightDecoder.h>
+#include <rfb/TightJPEGEncoder.h>
+#include <fstream>
+
+namespace suite {
+  rfb::PixelFormat pf(32, 24, false, true,
+                      255, 255, 255, 0, 8, 16);
+
+  JPEGDecoder::JPEGDecoder(int quality, int subsampling)
+    : ImageDecoder(JPEG), quality_(quality), subsampling_(subsampling)
+  {
+  }
+
+  JPEGDecoder::~JPEGDecoder()
+  {
+  }
+
+
+  Image* JPEGDecoder::decodeImageFromFile(std::string)
+  {
+    throw std::logic_error("method not implemented");
+  }
+
+  Image* JPEGDecoder::decodeImageFromMemory(uint8_t *data, int width,
+                                            int height, int size,
+                                            int x_offset, int y_offset)
+  {
+    rfb::Rect rect;
+    uint8_t* buffer;
+    Image* image;
+    rfb::JpegDecompressor jd;
+
+    rect.tl.x = x_offset;
+    rect.tl.y = y_offset;
+    rect.br.x = x_offset + width;
+    rect.br.y = y_offset + height;
+
+    buffer = new uint8_t[width * height * 4];
+
+    jd.decompress(data, size, buffer, width, rect, pf);
+
+#ifdef _DEBUG
+    measurePixelRate(width, height, 4);
+#endif // _DEBUG
+
+    image = new Image(width, height, buffer, size, x_offset, y_offset);
+    return image;
+  }
+
+  void JPEGDecoder::encodeImageTofile(const uint8_t *data, int width,
+                                      int height, std::string filename)
+  {
+    rfb::Rect rect;
+    std::ofstream of;
+    rfb::JpegCompressor jc;
+
+    rect.tl.x = 0;
+    rect.tl.y = 0;
+    rect.br.x = width;
+    rect.br.y = height;
+
+    jc.clear();
+    jc.compress(data, width, rect, pf, quality_, subsampling_);
+
+    of.open(filename);
+    of.write((char*)jc.data(), jc.length());
+    of.close();
+  }
+
+  Image* JPEGDecoder::encodeImageToMemory(const uint8_t *data, int width,
+                                          int height, int x_offset,
+                                          int y_offset)
+  {
+    rfb::Rect rect;
+    uint8_t *bufferCopy;
+    rfb::JpegCompressor jc;
+    Image* image;
+
+    rect.tl.x = x_offset;
+    rect.tl.y = y_offset;
+    rect.br.x = x_offset + width;
+    rect.br.y = y_offset + height;
+
+    jc.clear();
+    jc.compress(data, width, rect, pf, quality_, subsampling_);
+
+    bufferCopy = new uint8_t[jc.length()];
+    memcpy(bufferCopy, jc.data(), jc.length());
+
+#ifdef _DEBUG
+    measurePixelRate(width, height, 4);
+#endif // _DEBUG
+
+    image = new Image(width, height, bufferCopy, jc.length(),
+                             x_offset, y_offset);
+    return image;
+  }
+}

tests/suite/io/FrameOutStream.h
@@ -1,0 +1,93 @@
+#ifndef __SUITE_FRAME_OUT_STREAM_H__
+#define __SUITE_FRAME_OUT_STREAM_H__
+
+#include "ImageUpdate.h"
+#include "../codec/ImageDecoder.h"
+#include <fstream>
+#include <mutex>
+
+namespace suite {
+
+  /* This class is used to create the output file of a recorded VNC session.
+     The file structure is as follows:
+
+      ______________________________________________________
+      |               encoding width height interval     \n| <-- File header
+      | imageSize width height x_offset y_offset frameTime | <-- Image metadata
+      |       lostArea overArea encodingTime margin      \n|
+      |****************************************************|
+      |****************************************************|
+      |****************** RAW IMAGE DATA ******************| <-- Raw dump of
+      |****************************************************|     image data.
+      |****************************************************|
+      |**************************************************\n|
+      | imageSize width height x_offset y_offset frameTime | <-- Next image
+      |       lostArea overArea encodingTime margin      \n|
+      |****************************************************|
+      |****************************************************|
+      |****************** RAW IMAGE DATA ******************|
+      |**************************************************\n|
+      |____________________________________________________|
+
+      FileStructure: {
+        encoding:     Which encoder was used.
+        width:        Framebuffer width.
+        height:       Framebuffer height.
+        interval:     Specified interval between frame updates (milliseconds).
+        -----------------------------------------------------------------------
+        imageSize:    Image size in bytes.
+        width:        Image width.
+        height:       Image height.
+        x_offset:     Image top left x-coordinate.
+        y_offset:     Image top left y-coordinate.
+        frameTime:    Time until next frame update.
+
+        lostArea:     How large of an "area" that was lost due to multiple
+                      DAMAGE events detected between frame updates.
+
+        overArea:     How much area was overdimensioned by coalescing multiple
+                      DAMAGE events into a single bouding retangle.
+
+        encodingTime: How long it took to encode the update (milliseconds).
+
+        margin:       How many milliseconds we have to spare before the next
+                      framebuffer update is recorded. Negative number indicates
+                      how long we exceeded our interval.
+        -----------------------------------------------------------------------
+        RAW IMAGE DATA: encoded image data.
+      }
+
+      The file metadata tells which encoding was used for the images, as well
+      as the framebuffer width & height, as well as the interval between each
+      frame update. The header must be parsed first before processing the rest
+      of the file. The file header ends with a '\n'. The image header has
+      information about the next image stored in the file and is also marked by
+      ending with a '\n'. On the next line, the image can be read by reading
+      imageSize many bytes from the file, using the corresponding encoder.
+      The raw image is also terminated with a '\n'. There is no way of knowing
+      how many images are stores in the file beforehand, you have to parse the
+      whole file to know that.
+  */
+
+  class FrameOutStream
+  {
+  public:
+    FrameOutStream(std::string filename, ImageDecoder* decoder);
+    ~FrameOutStream();
+
+    // Assumes ownership if ImageUpdate
+    void addUpdate(ImageUpdate* update);
+    void addUpdate(uint8_t* data, int width, int height, int x_offset,
+                   int y_offset, int size);
+
+    void writeHeader(int width, int height, double interval);
+  private:
+    std::ofstream file;
+    std::mutex lock; // In case updates are encoded in parallel.
+    bool headerWritten;
+    const std::string decoder_;
+    std::chrono::steady_clock::time_point lastFrameTime;
+  };
+}
+
+#endif // __SUITE_FRAME_OUT_STREAM_H__

tests/suite/io/ImageUpdate.cxx
@@ -1,0 +1,13 @@
+#include "ImageUpdate.h"
+
+namespace suite {
+  ImageUpdate::ImageUpdate(Image* image_, ImageUpdateStats stats_)
+    : image(image_), stats(stats_)
+  {
+  }
+
+  ImageUpdate::~ImageUpdate()
+  {
+    delete image;
+  }
+}

tests/suite/io/ImageUpdate.h
@@ -1,0 +1,20 @@
+#ifndef __SUITE_IMAGE_UPDATE_H__
+#define __SUITE_IMAGE_UPDATE_H__
+
+#include "../codec/Image.h"
+#include "../recorder/util.h"
+
+namespace suite {
+
+  // Represents a framebuffer update. Takes ownership of the image.
+  // FIXME: Add time data so that updates can be played in "realtime".
+  struct ImageUpdate
+  {
+    ImageUpdate(Image* image, ImageUpdateStats stats_ = ImageUpdateStats{});
+    ~ImageUpdate();
+    Image* image;
+    ImageUpdateStats stats;
+  };
+}
+
+#endif // __SUITE_IMAGE_UPDATE_H__